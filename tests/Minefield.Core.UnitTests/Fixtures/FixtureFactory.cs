f79a170f9eb6e5a9e62040b47dc991b388364fb8
mark robinson <mark.robinson@education.gov.uk>
2023-02-09 21:23:28 +0000

    code tidy up

--- tests/Minefield.Core.UnitTests/Fixtures/FixtureFactory.cs
+++ tests/Minefield.Core.UnitTests/Fixtures/FixtureFactory.cs
@@ -27,25 +27,9 @@ internal static class FixtureFactory
     private static IServiceCollection GetBaseServiceCollection()
     {
         var serviceCollection = new ServiceCollection()
-            .AddSingleton(s => Mock.Of<IGameConfiguration>(t => t.Lives == 3 && t.NoOfMines == 20)) // need to default lives && NoOfMines
-                                                                                                    //.AddSingleton(s => SetupMockConfig()) 
+            .AddSingleton(s => Mock.Of<IGameConfiguration>(t => t.Lives == 3 && t.NoOfMines == 10)) // need to default lives && NoOfMines
             .AddCore(); //IMineGenerator mineGenerator setup here !!
 
         return serviceCollection;
     }
-
-    //private static Mock<IGameConfiguration> SetupMockConfig()
-    //{
-    //    var userMock = new Mock<IGameConfiguration>().SetupAllProperties();
-
-    //    SetPropertiesOfUser(userMock.Object);
-
-    //    return userMock;
-    //}
-
-    //private static void SetPropertiesOfUser(IGameConfiguration config)
-    //{
-    //    config.Lives = 3;
-    //    config.NoOfMines = 20;
-    //}
 }
