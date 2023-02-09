8ffd3d92d7827715e1994c7a2887c046a31adfee
mark robinson <mark.robinson@education.gov.uk>
2023-02-09 19:08:59 +0000

    added service provider

---
+++ tests/Minefield.Core.UnitTests/Common/TestBase.cs
@@ -0,0 +1,35 @@
+﻿namespace Minefield.Core.UnitTests.Common;
+
+public class TestBase
+{
+    public IServiceProvider ServiceProvider = null!;
+
+    [OneTimeSetUp]
+    public async Task Init()
+    {
+        ServiceProvider = Fixtures.FixtureFactory.CreateFixture();
+    }
+
+    /// <summary>
+    /// Code here would be executed before every test.
+    /// </summary>
+    //[SetUp]
+    //public async Task SetUp()
+    //{
+    //}
+
+    /// <summary>
+    /// Code here would be executed after every test.
+    /// </summary>
+    //[TearDown]
+    //public void TearDown()
+    //{
+    //    //
+    //}
+
+    //[OneTimeTearDown]
+    // public async Task ClearDown()
+    // {
+    // DestroyDbAndMocks();
+    // }
+}
